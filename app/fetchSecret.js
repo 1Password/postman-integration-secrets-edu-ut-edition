9b759b8ba3bea05160f09c0175c0e2b20f428fae
Chirag Rana <chirag.rana@mail.utoronto.ca>
2022-11-12 17:25:52 -0500

    added basic auth fetch from 1password cli

--- app/fetchSecret.js
+++ app/fetchSecret.js
@@ -2,7 +2,7 @@ import { inject, validateCli } from "@1password/op-js";
 
 /**
  * Fetch secret from 1Password CLI
- * @param {any} program - CLI
+ * @param {any} program - CLI (for logging errors)
  * @param {string} secretPath - Path in 1Password to secret
  * @returns - Secret
  */
@@ -14,6 +14,33 @@ export async function fetchSecret(program, secretPath) {
     program.error(error.message);
     return ;
   }
-  const data = inject.data(secretPath);
-  return data;
+
+  try {
+    const data = inject.data(secretPath);
+    return data;
+  } catch(error) {
+    return ;
+  }
+  
+}
+
+/**
+ * Fetch Basic Authentication credentials from 1Password CLI
+ * @param {any} program - CLI (for logging errors)
+ * @param {string} secretPath - Path in 1Password to secret item
+ * @returns - Basic Authentication credentials
+ */
+ export async function fetchBasicAuth(program, secretItemPath) {
+  
+  const usernameField = `${secretItemPath.replace(/\/$/, "")}/username`;
+  const username = await fetchSecret(program, usernameField);
+  const passwordField = `${secretItemPath.replace(/\/$/, "")}/password`; 
+  const password = await fetchSecret(program, passwordField);
+
+  if (!password || !username) {
+    program.error("Invalid Secret Path");
+    return {};
+  }
+
+  return { username, password };
 }
