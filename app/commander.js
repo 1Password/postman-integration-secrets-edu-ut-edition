1dd60c601ba50480eb2aadb8eab2e56878b0a0db
Chirag Rana <chirag.rana@mail.utoronto.ca>
2022-11-27 12:24:06 -0500

    updated commander with new inject secrets command

--- app/commander.js
+++ app/commander.js
@@ -1,5 +1,8 @@
 import { Command } from 'commander';
+import { injectSecretIntoPostman } from './injectSecret.js';
 import {runNewman} from "./runNewman.js";
+import { selectSecrets } from './selectSecrets.js';
+
 const program = new Command();
 
 program
@@ -22,5 +25,30 @@ program.command('run-collection')
     console.log('options:', options.mode);
     runNewman(program, collection, secret);
   });
+
+program.command('inject-secrets')
+  .description('Inject Secrets into Postman Environment ')
+  .option('-s, --postman-secret <secret>', '1Password secret path to Postman API Credential. It can also be provided as an environment variable: POSTMAN_API_CREDENTIAL')
+  .option('-e, --env <env-name>', '(Optional) name of Postman environment to inject secrets into', '1password-secrets')
+  .option('-r, --replace-env', '(Optional) replace the entire Postman environment with secrets instead of merging (default behaviour)')
+  .action(async (options) => {
+    let postmanSecret;
+    if(options.postmanSecret) {
+      const re = new RegExp('op:\/\/')
+      if(!re.test(options.postmanSecret)){
+        program.error('Error: invalid secret path');
+        return;
+      }
+      postmanSecret = options.postmanSecret;
+    } else if(process.env.POSTMAN_API_CREDENTIAL ) {
+      postmanSecret = process.env.POSTMAN_API_CREDENTIAL;
+    } else {
+      program.error("No Postman API Credential specified.");
+      return;
+    }
+    const secrets = await selectSecrets(program);
+    await injectSecretIntoPostman(program, postmanSecret, secrets, {envName: options.env, replaceEnv: !!options.replaceEnv})
+  });
+
 
 program.parse();
