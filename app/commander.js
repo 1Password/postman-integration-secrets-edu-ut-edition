f023f95af3b467efe8e4a2b7dddd8464be4dbad9
Yiyun Ding <amydingyiyun@gmail.com>
2022-11-27 16:14:07 -0500

    log at test mode

--- app/commander.js
+++ app/commander.js
@@ -11,8 +11,8 @@ program
 
 program.command('run-collection')
   .description('Run Postman collection with 1password secret')
-  .option('--mode <char>, -m <char>', 'Specify if it is for testing(t) or production(p)', 'p')
-  .option('--type <char>, -t <char>', 'Specify the type of authorization', 'NoAuth')
+  .option('--mode <char>', 'Specify if it is for testing(t) or production(p)', 'p')
+  .option('--type <char>', 'Specify the type of authorization', 'NoAuth')
   .argument('<secret>', '1password secret resource path')
   .argument('<collection>', 'collection configuration to insert')
   .action((secret, collection, options) => {
@@ -26,10 +26,13 @@ program.command('run-collection')
       throw 'Error: type not valid. Allowed types are NoAuth, ApiKey, Bearer\n' +
             'Basic, Digest, OAuth1, OAuth2, Hawk, AWS, NTLM, Akamai';
     }
-    console.log('secret:', secret);
-    console.log('collection:', collection);
-    console.log('options:', options.mode);
-    console.log('type:', options.type);
+    if(options.mode == 't'){
+      console.log('secret:', secret);
+      console.log('collection:', collection);
+      console.log('options:', options.mode);
+      console.log('type:', options.type);
+    }
+    
     runNewman(program, collection, secret, type_lower);
   });
 
